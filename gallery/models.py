9838a9f3d956fbe397b8bd5a20c20e9fb83ffe09
@laoliu <675050748@qq.com>
2019-02-24 15:19:10 +0800

    完善了Gallery APP

--- gallery/models.py
+++ gallery/models.py
@@ -4,4 +4,9 @@ from django.db import models
 
 
 class Gallery(models.Model):
-    description = models.CharField(max_length=100)
+    description = models.CharField(default='', max_length=100)
+    image = models.ImageField(default='',upload_to='image/')
+    title = models.CharField(default= '标题',max_length=50)
+
+    def __str__(self):
+        return self.title
