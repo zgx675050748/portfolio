20afff3ae16c70628a73f4b816c690ceb28f1ff5
@laoliu <675050748@qq.com>
2019-02-25 10:19:47 +0800

    美化博客

--- blog/models.py
+++ blog/models.py
@@ -7,8 +7,11 @@ class Blog(models.Model):
     title = models.CharField(default='文章标题', max_length=50)
     date = models.DateField()
     image = models.ImageField(default='default.png')
-    text = models.CharField(default='正文', max_length=100)
+    text = models.CharField(default='正文', max_length=10000)
 
 
     def __str__(self):
         return self.title
+
+    def short_text(self):
+        return self.text[:70] + '...'
